32af24faceb70fe609c420396e010d1b65b081f9
e <Q1581897458@163.com>
2020-09-04 19:49:49 +0800

    第er次提啊

--- b2c/b2c-item/b2c-item-service/src/main/java/com/mr/ItemApplication.java
+++ b2c/b2c-item/b2c-item-service/src/main/java/com/mr/ItemApplication.java
@@ -12,5 +12,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ItemApplication {
     public static void main(String[] args) {
         SpringApplication.run(ItemApplication.class, args);
+
+        System.out.println("aaaa");
+
     }
 }
